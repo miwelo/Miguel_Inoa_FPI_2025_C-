ffdf64262b3662a0e3b6210c4e5ed8af9ee34147
Miguel Inoa <20250081@itla.edu.do>
2025-09-18 22:37:24 -0400

    Error #5421 fixed

---
+++ 1-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+    INVERSION DE DATOS
+*/
+
+void main(void)
+{
+    int A, B, C, D;
+    printf("Ingrese cuatro datos de tipo entero: ");
+    scanf("%d %d %d %d", &A, &B, &C, &D);
+    printf("\n %d %d %d %d", D, C, B, A);
+}
